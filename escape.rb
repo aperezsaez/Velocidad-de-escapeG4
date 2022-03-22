6c8d7cd7560eba7b9b271cfa8c00876d89e67c0c
aperezsaez <alex.perez.22@live.com>
2022-03-22 19:11:08 -0300

    escape.rb finished

---
+++ escape.rb
@@ -0,0 +1,13 @@
+# Se pide crear el programa escape.rb donde el usuario ingrese la gravedad y el radio, y como
+# resultado obtenga la velocidad de escape (ocupar la fórmula).
+# 𝑉𝑒 = sqrt(2𝑔r)
+# El programa debe poder ejecutarse como: ruby escape.rb 9.8 6371
+#! el usuario debe ingresar gravedad y radio
+g = ARGV[0].to_i
+r = ARGV[1].to_i
+#! transformar r en kilometros
+r = 1000*r
+#!aplicar formula
+ve = Math.sqrt(2*g*r)
+#!imprimir resultado en pantalla
+puts ve
